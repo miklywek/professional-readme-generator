0a7c6e4cd97f96b1b6cf29789a8f013a760fa3cd
miklywek <miklywek@gmail.com>
2022-12-04 15:10:09 +0200

    add file

---
+++ index.js
@@ -0,0 +1,194 @@
+// // TODO: Include packages needed for this application
+// import inquirer from "inquirer";
+// import generateMarkdown from "./utils/generateMarkdown.js";
+
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown.js");
+const fs = require("fs");
+
+// TODO: Create an array of questions for user input
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project? (Required)",
+    validate: (titleInput) => {
+      if (titleInput) {
+        return true;
+      } else {
+        console.log("Please enter your title!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "githubUsername",
+    message: "What is your GitHub Username? (Required)",
+    validate: (githubInput) => {
+      if (githubInput) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address? (Required)",
+    validate: (emailInput) => {
+      if (emailInput) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
+  //   {
+  //     type: "input",
+  //     name: "what",
+  //     message: "What is your project and what problem will it solve? (Required)",
+  //     validate: (whatInput) => {
+  //       if (whatInput) {
+  //         return true;
+  //       } else {
+  //         return false;
+  //       }
+  //     },
+  //   },
+  //   {
+  //     type: "input",
+  //     name: "why",
+  //     message: "Why did you create this project? (Required)",
+  //     validate: (whyInput) => {
+  //       if (whyInput) {
+  //         return true;
+  //       } else {
+  //         return false;
+  //       }
+  //     },
+  //   },
+  //   {
+  //     type: "input",
+  //     name: "how",
+  //     message: "How will someone use this? (Required)",
+  //     validate: (howInput) => {
+  //       if (howInput) {
+  //         return true;
+  //       } else {
+  //         return false;
+  //       }
+  //     },
+  //   },
+  //   {
+  //     type: "input",
+  //     name: "installation",
+  //     message:
+  //       "Please provide step-by-step installation instructions for your project. (Required)",
+  //     validate: (instalInput) => {
+  //       if (instalInput) {
+  //         return true;
+  //       } else {
+  //         return false;
+  //       }
+  //     },
+  //   },
+  //   {
+  //     type: "input",
+  //     name: "usage",
+  //     message: "Please provide instructions and examples for use. (Required)",
+  //     validate: (usageInput) => {
+  //       if (usageInput) {
+  //         return true;
+  //       } else {
+  //         return false;
+  //       }
+  //     },
+  //   },
+  //   {
+  //     type: "list",
+  //     name: "license",
+  //     message: "Which license will you use for your project?",
+  //     choices: ["agpl", "apache", "mit", "no license"],
+  //   },
+  //   {
+  //     type: "confirm",
+  //     name: "confirmContributers",
+  //     message: "Would you like to allow other developers to contribute?",
+  //     default: true,
+  //   },
+  //   {
+  //     type: "input",
+  //     name: "contribute",
+  //     message: "Please provide guidelines for contributing. (Required)",
+  //     when: ({ confirmContributers }) => {
+  //       if (confirmContributers) {
+  //         return true;
+  //       } else {
+  //         return false;
+  //       }
+  //     },
+  //     validate: (contributerInput) => {
+  //       if (contributerInput) {
+  //         return true;
+  //       } else {
+  //         console.log("Please enter contributer guidelines!");
+  //         return false;
+  //       }
+  //     },
+  //   },
+  {
+    type: "input",
+    name: "test",
+    message: "Please provide instructions on how to test the app. (Required)",
+    validate: (testInput) => {
+      if (testInput) {
+        return true;
+      } else {
+        console.log("Please enter your use test instructions!");
+        return false;
+      }
+    },
+  },
+];
+
+// TODO: Create a function to write README file
+const writeFile = (fileContent) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile("./dist/generate.md", fileContent, (err) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve({
+        ok: true,
+        message: "File created!",
+      });
+    });
+  });
+};
+
+// TODO: Create a function to initialize app
+const init = () => {
+  return inquirer.prompt(questions).then((readmeData) => {
+    return readmeData;
+  });
+};
+
+// Function call to initialize app
+init()
+  .then((readmeData) => {
+    console.log(readmeData);
+    return generateMarkdown(readmeData);
+  })
+  .then((pageMD) => {
+    console.log(pageMD);
+    return writeFile(pageMD);
+  })
+  .then((writeFileResponse) => {
+    console.log(writeFileResponse.message);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
